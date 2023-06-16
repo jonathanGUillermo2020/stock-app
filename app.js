81cb65892630375dad154be32ebc3beafb83cb0d
jonathanguillermo2020 <jonathanguillermo.medina.ordaz2124@cbtis270.edu.mx>
2023-06-15 23:50:52 -0600

    conexion con mongo db

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const path = require('path')
 require('dotenv').config()
+
 const app = express()
 
-app.get('/',(req, res) => {
-console.log('peticion recibida')
+mongoose.connect(`mongodb+srv://jonathanguillermomedinaordaz2124:${process.env.MONGO_DB_PASS}@development.7wvyqcv.mongodb.net/?retryWrites=true&w=majority`)
 
-   res.send('<h1>Hola mundo!</h1>')
+.then(result => {
+    app.listen(PORT,() =>{
+
+        console.log(`Servidor escuchando en el puerto ${PORT}`)
+    })    
+    console.log('Conexion exitosa a la base de BBDD')} )
+
+const productSchema = mongoose.Schema({
+    name: { type: String, required: true},
+    price: Number,
+},
+{timestamps: true }
+)
+
+const Product = mongoose.model('product',productSchema)
+
+app.use(express.json())
+
+
+app.post('/api/v1/products',(req, res) => {
+
+const newProduct = new Product(req.body)
+
+newProduct.save().then(result => {
+
+    
 })
+
+res.status(201).json({ok: true})
+    })
+
+app.use(express.static(path.join(__dirname, 'publico')))
+
 
 const PORT = process.env.PORT 
 
-
-app.listen(PORT,() =>{
-
-    console.log(`Servidor escuchando en el puerto ${PORT}`)
-})
 
